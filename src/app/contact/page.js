183738bc8bf96e4fcd6bd5cf09e905bef15f5ab5
Rezky jayadi saputra <rezkyjayadis@gmail.com>
2024-09-09 19:27:55 +0700

    update contact

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,13 +1,15 @@
 export default function contact(){
     return(
         <>
-        <h2>Get in touch</h2>
-        <p>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum 
+        <h2 className="text-center text-3x1 w-full">Get in touch</h2>
+        <p className="text-center margin-0 mx-auto w-2/3">
+            lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum 
             lorem ipsum lorem ipsum
             </p>
-        <div>
-            <div>Kiri</div>
-            <div>Kanan</div>
+
+        <div className="grid grid-cols-2 gap-4">
+            <div className="border">kiri</div>
+            <div className="border">kanan</div>
         </div>
         </>
     );
