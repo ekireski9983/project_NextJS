06357a1af0ce70e0ddec9dc8604b3dbe5ba518ee
Rezky jayadi saputra <rezkyjayadis@gmail.com>
2024-10-07 20:02:16 +0700

    update work

--- src/app/(auth)/admin/work/page.js
+++ src/app/(auth)/admin/work/page.js
@@ -1,74 +1,116 @@
+"use client"
+import { useState } from 'react';
 import Card from '../../../../components/card';
 
 export default function AdminWork() {
+  const [data, setData] = useState({
+    title:'',
+    employeType:'',
+    companyName:'',
+    location:'',
+    startDate:'',
+    endDate:'',
+  });
+  
+
   const optEmployeType = [
     {label:'Full Time', value:'full-time'},
     {label:'Part Time', value:'part-time'},
     {label:'Contract', value:'contract'},
     {label:'Internship', value:'internship'}
   ]
+
   const optLocation = [
-    {label:'onsite', value:'onsite'},
-    {label:'work-from-home', value:'work-from-home'},
-    {label:'hybird', value:'hybird'},
+    {label:'Onsite', value:'onsite'},
+    {label:'WFH', value:'wfh'},
   ]
 
-    return (<>
-        <Card title="Work Form" className="pb-5">
-          <div className="w-full my-2">
-              <label>Title</label>
-              <input type="text" className="w-full border my-input-text"/>
-          </div>
+  const inputHandler= (e) =>{
+    setData({...data, [e.target.name]: e.target.value })
+  }
 
-          <div className="w-full my-2">
-              <label>Employe Type</label>
-              <select  className="w-full border my-input-text">
-                {
-                  optEmployeType && 
-                    optEmployeType.map((item)=>
-                      <option value={item.value}>{item.label}</option>
-                    )
-                }
-              </select>
-          </div>
+  return (<>
+      <Card title="Work Form" className="pb-5">
+        <div className="w-full my-2">
+            <label>Title</label> 
+            <input 
+              type="text" 
+              placeholder="Ex: Retail Sales Manager"
+              name='title'
+              value={data.title}
+              onChange={inputHandler}
+              className="w-full border my-input-text"/>
+        </div>
 
-          <div className="w-full my-2">
-              <label>Company Name</label>
-              <input type="text" className="w-full border my-input-text"/>
-          </div>
-
-          <div className="w-full my-2">
-              <label>Location</label>
-              <select  className="w-full border my-input-text">
+        <div className="w-full my-2">
+            <label>Employe Type</label>
+            <select  
+              name='employeType' 
+              placeholder="Ex: Microsoft"
+              onChange={inputHandler}
+              className="w-full border my-input-text">
               {
-                  optLocation && 
-                    optLocation.map((item)=>
-                      <option value={item.value}>{item.label}</option>
-                    )
-                }
-                <option>Onsite</option>
-              </select>
-          </div>
+                optEmployeType && 
+                  optEmployeType.map((item, key)=>
+                    <option key={key} value={item.value}>{item.label}</option>
+                  )
+              }
+            </select>
+        </div>
 
-          <div className="w-full my-2">
-              <label>Start Date</label>
-              <input type="date" className="w-full border my-input-text"/>
-          </div>
+        <div className="w-full my-2">
+            <label>Company Name</label>
+            <input 
+              name='companyName' 
+              placeholder="Ex: Pertamina"
+              type="text" 
+              onChange={inputHandler}
+              className="w-full border my-input-text"/>
+        </div>
 
-          <div className="w-full my-2">
-              <label>End Date</label>
-              <input type="date" className="w-full border my-input-text"/>
-          </div>
+        <div className="w-full my-2">
+            <label>Location</label>
+            <select 
+              name='location'
+              placeholder="Ex: Indonesia,Jakarta"
+              onChange={inputHandler}
+              className="w-full border my-input-text">
+              {
+                optLocation && 
+                optLocation.map((item, key)=>
+                    <option key={key} value={item.value}>{item.label}</option>
+                  )
+              }
+            </select>
+        </div>
 
-          <button className="mx-1 h-9 items-center justify-center px-4  rounded-md bg-amber-500">
-              <label>Submit Data</label>
-          </button>
-        </Card>
-        
-        <Card title="List of Work" style="mt-5">
-          asdf
-        </Card>
-      </>
-    );
+        <div className="w-full my-2">
+            <label>Start Date</label>
+            <input 
+              name='startDate'
+              onChange={inputHandler}
+              type="date" 
+              className="w-full border my-input-text"/>
+        </div>
+
+        <div className="w-full my-2">
+            <label>End Date</label>
+            <input 
+              name='endDate'
+              onChange={inputHandler}
+              type="date" 
+              className="w-full border my-input-text"/>
+        </div>
+
+        <button className="mx-1 h-9 items-center justify-center px-4  rounded-md bg-amber-500">
+            <label>Submit Data</label>
+        </button>
+      </Card>
+      
+      <Card title="List of Work" style="mt-5">
+        asdf
+      </Card>
+    </>
+  );
 }
   
