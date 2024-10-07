33d49f7f17601ca311c9c2ffdd3f80a7135680f5
Rezky jayadi saputra <rezkyjayadis@gmail.com>
2024-10-07 20:15:16 +0700

    update work dan dashboard

--- src/app/(auth)/admin/work/page.js
+++ src/app/(auth)/admin/work/page.js
@@ -102,12 +102,12 @@ export default function AdminWork() {
               className="w-full border my-input-text"/>
         </div>
 
-        <button className="mx-1 h-9 items-center justify-center px-4  rounded-md bg-blue-500 text-white">
+        <button className="mx-1 h-9 items-center justify-center px-4 rounded-md bg-blue-500 text-white">
             <label>Submit Data</label>
         </button>
 
-        <button className="mx-1 h-9 items-center justify-center px-4  rounded-md bg-red-500 text-white">
-            <label>reset data</label>
+        <button className="mx-1 h-9 items-center justify-center px-4 rounded-md bg-red-500 text-white">
+            <label>Reset data</label>
         </button>
       </Card>
       
--- src/app/(public)/page.js
+++ src/app/(public)/page.js
@@ -8,7 +8,7 @@ export default function Dashboard() {
                         <h3 className="text-left text-2xl w-full font-bold">I Am <span class="text-blue-600">Rezky jayadi saputra</span></h3>
                         <p className="w-1/2">Saya seorang mahasiswa aktif Politeknik LP3I Jakarta Semester 5</p>
                       <div className="w-full py-2">
-                            <button className="mx-1 h-9 items-center justify-center px-4  rounded-md bg-amber-500">
+                            <button className="mx-1 h-9 items-center justify-center px-4 rounded-md bg-blue-500 text-white">
                                 <label>Contact</label>
                             </button>
                         </div>
