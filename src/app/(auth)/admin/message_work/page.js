d70078633f2c7732b236d41b2908fce353d3025f
Rezky jayadi saputra <rezkyjayadis@gmail.com>
2024-10-21 21:06:15 +0700

    update work database dan admin work

--- src/app/(auth)/admin/message_work/page.js
+++ src/app/(auth)/admin/message_work/page.js
@@ -12,7 +12,20 @@ export default function WorkList(){
         setData(data.data)
         setLoading(false)
     }
-    
+
+    const onDeleteItem = async (id)=>{
+        const request = {
+            deleted_id:id
+        }
+        
+        let res = await fetch(`/api/work`,{
+            method:'DELETE',
+            body: JSON.stringify(request),
+        } )
+
+        onLoadData()
+    }
+
     useEffect(() => {
         onLoadData()
     }, [])
@@ -48,10 +61,10 @@ export default function WorkList(){
                                 <td className='p-2 '>{item.endDate}</td>
                                 <td className='p-2 '>
                                     <div className="inline-flex text-[12px]">
-                                        <button className=" bg-green-300 hover:bg-green-400 text-gray-800 py-2 px-4 rounded-l">
+                                        <button className=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                                             Edit
                                         </button>
-                                        <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-4 rounded-r">
+                                        <button onClick={()=>onDeleteItem(item._id)} className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
                                             Delete
                                         </button>
                                     </div>
