a3bd61c6a3e25b6e059ea076f8ac32c2317bfce8
Rezky jayadi saputra <rezkyjayadis@gmail.com>
2024-10-21 19:25:47 +0700

    update work admin

---
+++ src/app/(auth)/admin/work/component/work-list.js
@@ -0,0 +1,42 @@
+export default function WorkList(){
+    return (
+        <>
+            <table className="table-auto">
+                <thead>
+                    <tr>
+                        <th className='p-2 border-b border-blue-gray-100 bg-gray-100'>#No</th>
+                        <th className='p-2 border-b border-blue-gray-100 bg-gray-100'>Title</th>
+                        <th className='p-2 border-b border-blue-gray-100 bg-gray-100'>Employe Type</th>
+                        <th className='p-2 border-b border-blue-gray-100 bg-gray-100'>Company Name</th>
+                        <th className='p-2 border-b border-blue-gray-100 bg-gray-100'>Location</th>
+                        <th className='p-2 border-b border-blue-gray-100 bg-gray-100'>Start Date</th>
+                        <th className='p-2 border-b border-blue-gray-100 bg-gray-100'>End Date</th>
+                        <th className='p-2 border-b border-blue-gray-100 bg-gray-100'>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+               
+                    <tr className='border-b border-blue-gray-50'>
+                        <td className='p-2 '>1</td>
+                        <td className='p-2 '>Jhon doe</td>
+                        <td className='p-2 '>jhondoe@mail.com</td>
+                        <td className='p-2 '>Loremipsum</td>
+                        <td className='p-2 '>loremipsum loremipsum loremipsum</td>
+                        <td className='p-2 '>loremipsum loremipsum loremipsum</td>
+                        <td className='p-2 '>loremipsum loremipsum loremipsum</td>
+                        <td className='p-2 '>
+                            <div class="inline-flex text-[12px]">
+                                <button class=" bg-green-300 hover:bg-green-400 text-gray-800 py-2 px-4 rounded-l">
+                                    Balas
+                                </button>
+                                <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-4 rounded-r">
+                                    Arsipkan
+                                </button>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </>
+    )
+}
