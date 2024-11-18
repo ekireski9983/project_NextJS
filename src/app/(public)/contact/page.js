c43aeaaf6cc8c8399ba54377f77182f113a93e37
Rezky jayadi saputra <rezkyjayadis@gmail.com>
2024-11-18 19:32:49 +0700

    add button cancel contact

--- src/app/(public)/contact/page.js
+++ src/app/(public)/contact/page.js
@@ -92,6 +92,9 @@ export default function Contact() {
                                 <button type='submit' className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                                     <label>Send Message</label>
                                 </button>
+                                <button type='reset' className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+                                    <label>cancel</label>
+                                </button>
                             </div>
                         </div>
                     </div>
