90c33c5e8b4ef558e70f54a9aa20c161c364682b
Rezky jayadi saputra <rezkyjayadis@gmail.com>
2024-11-18 19:52:02 +0700

    add blogs

--- src/app/(public)/contact/page.js
+++ src/app/(public)/contact/page.js
@@ -93,7 +93,7 @@ export default function Contact() {
                                     <label>Send Message</label>
                                 </button>
                                 <button type='reset' className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
-                                    <label>cancel</label>
+                                    <label>Cancel</label>
                                 </button>
                             </div>
                         </div>
