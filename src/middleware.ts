9595fb226c1c1d88230c67754fbd3b760e794875
Rezky jayadi saputra <rezkyjayadis@gmail.com>
2024-11-18 20:38:21 +0700

    update blogs

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,41 @@
 import type { NextRequest } from 'next/server'
 import {  NextResponse } from 'next/server'
+import {  jwtVerify } from 'jose'
 
 const protectedRoutes = [
     '/admin',
     "/admin/work",
     "/admin/message"
-
 ]
 
 export default async function middleware(req: NextRequest) {
-    const cookie = req.cookies.get(`${process.env.AUTH_COOKIE_NAME}`)
-    const path = req.nextUrl.pathname
-    const isProtectedRoute = protectedRoutes.includes(path)
 
-    if (isProtectedRoute && !cookie) {
-        return NextResponse.redirect(new URL('/login', req.nextUrl))
-    }
+        const cookie = await req.cookies.get(`${process.env.AUTH_COOKIE_NAME}`) || ''
+        const path = req.nextUrl.pathname
+        const isProtectedRoute = protectedRoutes.includes(path)
+        let userId:any = ''
+        if(cookie?.value){
+            
+            // const secretKey = process.env.SESSION_SECRET
+            // const encodedKey = new TextEncoder().encode(secretKey)
+            try{
+                // const { payload } = await jwtVerify(cookie?.value, encodedKey, {
+                //     algorithms: ['HS256'],
+                // })
+                // userId = payload.id || ''
+                userId = cookie?.value
+            }catch(er){
+                console.log('er=>',er)
+                userId = ""
+            }
+        }
 
-    return NextResponse.next()
+        console.log('isProtectedRoute', isProtectedRoute)
+        console.log('userId', userId)
+        if (isProtectedRoute &&  userId ==='') {
+            return NextResponse.redirect(new URL('/login', req.nextUrl))
+        }
+
+        return NextResponse.next()
+   
 }
