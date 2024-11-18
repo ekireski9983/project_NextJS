44dd521da243bdddbf5e48d0f4f3e9a36aa5f0a0
Rezky jayadi saputra <rezkyjayadis@gmail.com>
2024-11-18 21:19:09 +0700

    update blogs

--- src/app/(auth)/admin/blogs/page.js
+++ src/app/(auth)/admin/blogs/page.js
@@ -15,9 +15,9 @@ export default function AdminBlogs() {
                 <thead>
                     <tr>
                         <th className='p-2 border-b border-blue-gray-100 bg-gray-100'>Title</th>
-                        <th className='p-2 border-b border-blue-gray-100 bg-gray-100'>Sub_Title</th>
+                        <th className='p-2 border-b border-blue-gray-100 bg-gray-100'>Sub Title</th>
                         <th className='p-2 border-b border-blue-gray-100 bg-gray-100'>Content</th>
-                        <th className='p-2 border-b border-blue-gray-100 bg-gray-100'>Created_at</th>
+                        <th className='p-2 border-b border-blue-gray-100 bg-gray-100'>Created at</th>
                         <th className='p-2 border-b border-blue-gray-100 bg-gray-100'>Action</th>
                     </tr>
                 </thead>
