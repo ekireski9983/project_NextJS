152529c96d3c5eeb6da8be1ecd022e64d1faee28
Rezky jayadi saputra <rezkyjayadis@gmail.com>
2024-11-25 21:16:31 +0700

    tiny mce

--- src/app/(auth)/admin/blogs/form/page.js
+++ src/app/(auth)/admin/blogs/form/page.js
@@ -2,8 +2,11 @@
 import Card from '../../../../../components/card';
 import ConfigDialog from '../../../../../components/ConfirmDialog'
 import { useState } from 'react'
+import { useRef } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
 
 export default function AdminBlogsForm() {
+    const editorRef = useRef(null);
     const [modal, setModal] = useState(false)
     const [modalTitle, setModalTitle] = useState("")
     const [modalMessage, setModalMessage] = useState("")
@@ -34,19 +37,23 @@ export default function AdminBlogsForm() {
 
     async function onSubmitData() {
         try{
-          let res = await fetch('/api/blogs', {
-            method:'POST',
-            body: JSON.stringify(data),
-          })
-          let resData = await res.json()
-          if(!resData.data){
-            throw Error(resData.message)
-          }
-          setModal(true)
-          setModalTitle('Info')
-          setModalMessage(resData.message)
-          
+            if (editorRef.current) {
+                const body = data
+                body.content = editorRef.current.getContent();
 
+                let res = await fetch('/api/blogs', {
+                    method:'POST',
+                    body: JSON.stringify(body),
+                })
+
+                let resData = await res.json()
+                if(!resData.data){
+                throw Error(resData.message)
+                }
+                setModal(true)
+                setModalTitle('Info')
+                setModalMessage(resData.message)
+            }
         }catch(err){
           console.error("ERR", err.message)
           setModal(true)
@@ -80,11 +87,26 @@ export default function AdminBlogsForm() {
 
             <div className="w-full my-2">
                 <label>Content</label>
-                    <input 
-                        name='content'
-                        value={data.content}
-                        onChange={inputHandler}
-                        className="w-full border my-input-text"/>
+                <Editor
+                    id='content'
+                    apiKey='hz9os6h0p1826jcqknks4q1fm8yl9khctaa7nmexkf0rnx2e'
+                    onInit={(_evt, editor) => editorRef.current = editor}
+                    initialValue={data.content}
+                    init={{
+                    height: 500,
+                    menubar: false,
+                    plugins: [
+                        'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+                        'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+                        'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
+                    ],
+                    toolbar: 'undo redo | blocks | ' +
+                        'bold italic forecolor | alignleft aligncenter ' +
+                        'alignright alignjustify | bullist numlist outdent indent | ' +
+                        'removeformat | help',
+                    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+                    }}
+                />
             </div>
 
             <button  className="btn-primary" onClick={onSubmitData}>
