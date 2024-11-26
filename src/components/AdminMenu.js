fe344364305cf18ee0d6a1cc483c8c1b918c1c82
Rezky jayadi saputra <rezkyjayadis@gmail.com>
2024-11-26 20:37:07 +0700

    add deploy

--- src/components/AdminMenu.js
+++ src/components/AdminMenu.js
@@ -33,9 +33,6 @@ export default function AdminMenu() {
                       <a className="btn-link" href="/admin/message">Messages</a>
                     </li>
                     <li className="mt-1">
-                      <a className="btn-link" href="/admin/message_work">Messages work</a>
-                    </li>
-                    <li className="mt-1">
                       <a className="btn-link" href="/admin/blogs">Blogs</a>
                     </li>
                     <li>
