0b7a4ccbcc0910d4e6f4342ffa94fcd35c7fd989
ekireski9983 <rezkyjayadis@gmail.com>
2024-12-13 12:35:29 +0700

    tes button

--- src/app/(auth)/admin/blogs/page.js
+++ src/app/(auth)/admin/blogs/page.js
@@ -127,7 +127,7 @@ export default function AdminBlogs() {
                                         <div className="inline-flex text-[12px]">
                                         <button 
                                             onClick={()=>gotoEditPage(item._id)}
-                                            className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                                            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                                             Edit
                                         </button>
                                         <button 
