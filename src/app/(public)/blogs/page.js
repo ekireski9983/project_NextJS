4ab128182ccc5cd6cb6b569df47305110f015add
ekireski9983 <rezkyjayadis@gmail.com>
2024-12-14 10:08:23 +0700

    edit blogs search public

--- src/app/(public)/blogs/page.js
+++ src/app/(public)/blogs/page.js
@@ -87,7 +87,7 @@ export default function Blogs() {
         Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.
         Velit officia consequat duis enim velit mollit. lorem ipsum
       </p>
-      <h1 className="text-2xl font-bold mb-4">Search Filter Example</h1>
+      <h1 className="text-2xl font-bold mb-4 text-center">Search Filter Example</h1>
       <div className="flex justify-center" >
 
       <form
