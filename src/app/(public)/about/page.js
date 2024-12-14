49b4cd6a437546512c7f6a4210a525e82d1b63d8
ekireski9983 <rezkyjayadis@gmail.com>
2024-12-14 22:10:50 +0700

    test about

--- src/app/(public)/about/page.js
+++ src/app/(public)/about/page.js
@@ -1,23 +1,9 @@
 export default function About() {
   
     return (
-      
-<ol classname="relative border-s border-gray-200 dark:border-gray-700">   
-<time classname="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Pendidikan</time>               
-    <li classname="mb-10 ms-4">
-        <div classname="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-        <time classname="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2019-2022</time>
-        <h3 classname="text-lg font-semibold text-gray-900 dark:text-white">SMKN 2 Jakarta</h3>
-        <p classname="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Jurusan multimedia</p>
-    </li>
-    <li classname="mb-10 ms-4">
-        <div classname="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-        <time classname="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Politeknik LP3I Jakarta</time>
-        <h3 classname="text-lg font-semibold text-gray-900 dark:text-white">2022-Sekarang</h3>
-        <p classname="text-base font-normal text-gray-500 dark:text-gray-400">Jurusan Manajamen informatika</p>
-    </li>
-</ol>
-
+      <div>
+        test About
+      </div>
     );
 }
   
